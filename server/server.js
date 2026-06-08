2d0f67ed7f9be75591fb5a2cfa7016f638e8afc0
Susovan <susovan.sahoo777@gmail.com>
2026-06-08 11:39:21 +0530

    Updated configurations

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ import app from './app.js';
 import { configDotenv } from 'dotenv';
 
 const port = process.env.PORT || 8080;
+const api_base = process.env.BACKEND_URL || `http://localhost:${port}`;
 
 app.listen(port, () => {
   console.log('🛜 Server started on port:', port);
-  console.log(`🔗 API base: http://localhost:${port}`);
+  console.log(`🔗 API base: ${api_base}`);
 });
